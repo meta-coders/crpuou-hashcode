5238aacf708a95bdb3ee9cd8b3423c423099e668
Rumiantsev Oleksii <rumiantsev.oleksii@gmail.com>
2019-02-28 22:01:26 +0200

    Minor optimization

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const union = (set1, set2) => set2.reduce(
   (acc, cur) => (acc.includes(cur) ? null : acc.push(cur), acc), set1
 );
 
-const findMaxInterestingImg = (dataset, tags) => {
+const findMaxInterestingImg = (dataset, img) => {
   let max = -1;
-  let maxVal = null;
+  let maxVal = { tags: [] };
 
   for (const [, val] of dataset) {
-    const comp = compare(tags, val.tags);
+    if (!val.vertical || img.id === val.id) continue;
+    const comp = compare(img.tags, val.tags);
     if (comp > max) {
       max = comp;
       maxVal = val;
@@ -29,48 +30,54 @@ const composeSlides = dataset => {
 
   dataset.forEach(img => {
     if (img.vertical) {
-      const img2 = findMaxInterestingImg(dataset, img.tags);
+      const img2 = findMaxInterestingImg(dataset, img);
       dataset.delete(img.id);
-      slides.set(slideId++, {
+      slides.set(slideId, {
+        id: slideId++,
         ids: [img.id, img2.id],
         tags: union(img.tags, img2.tags),
       });
     } else {
-      slides.set(slideId++, { ids: [img.id], tags: img.tags });
+      slides.set(slideId, {
+        id: slideId++,
+        ids: [img.id],
+        tags: img.tags
+      });
     }
   });
 
   return slides;
 };
 
-const findMaxInterestingSlide = (slides, tags) => {
+const findMaxInterestingSlide = (slides, added, curr) => {
   let max = -1;
-  let maxId = null;
   let maxSlide = null;
 
   for (const [id, slide] of slides) {
-    const comp = compare(tags, slide.tags);
+    if (added.has(id) || id === curr.id) continue;
+    const comp = compare(curr.tags, slide.tags);
     if (comp > max) {
       max = comp;
-      maxId = id;
       maxSlide = slide;
     }
   }
 
-  slides.delete(maxId);
   return maxSlide;
 };
 
 const makeTransitions = slides => {
   const transitions = [];
-
   transitions.push(slides.get(0));
-  slides.delete(0);
 
-  while (slides.size !== 0) {
+  const added = new Set();
+  added.add(0);
+
+  while (slides.size !== added.size) {
+    console.log(transitions.length);
     const prev = transitions[transitions.length - 1];
-    const next = findMaxInterestingSlide(slides, prev.tags);
+    const next = findMaxInterestingSlide(slides, added, prev);
     transitions.push(next);
+    added.add(next.id);
   }
 
   return transitions;
@@ -83,4 +90,4 @@ const run = async filename => {
   await save('./output/' + filename + '.out', sorted);
 };
 
-run('a_example.txt').catch(console.error);
+run('b_lovely_landscapes').catch(console.error);
